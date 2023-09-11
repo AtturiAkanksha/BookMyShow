5b3f83ab292bee0aa4faa61973ac93ffc74b84f8
Akanksha Atturi <akanksha.a@Technovert.com>
2023-09-11 14:56:15 +0530

    formatted, removed unnecessary packages and imports.

--- BookMyShow.API/BookMyShow.API/Controllers/MoviesController.cs
+++ BookMyShow.API/BookMyShow.API/Controllers/MoviesController.cs
@@ -2,7 +2,6 @@
 using BookMyShow.Services.Contracts;
 using BookMyShow.DomainModels;
 using BookMyShow.API.ResponseDTOs;
-using Microsoft.AspNetCore.Authorization;
 
 namespace BookMyShowWeb.Controllers
 {
